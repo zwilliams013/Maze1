c0e4c450f62495f7f319f9d2536ec39a926afe3f
Zack Williams <zwilliams013@gmail.com>
2016-03-06 19:10:10 -0500

    Initialization

---
+++ Maze1.cpp
@@ -0,0 +1,28 @@
+#include "Maze1.h"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+Maze1::~Maze1()
+{
+  // FILL IN THE CODE FOR THIS METHOD
+
+}
+
+void Maze1::find_exit()
+// Use the DFS method to fill the path stack with the exit path Positions
+// If there is no path to the exit, then after execution of this function
+// the stack path should be empty
+// This function does no input and no output
+{
+  // FILL IN THE CODE FOR THIS METHOD
+  Position nbr, current;
+  direction d;
+
+
+}
+
+
+
+
